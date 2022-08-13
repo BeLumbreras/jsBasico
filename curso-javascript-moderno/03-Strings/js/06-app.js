70462a08a5ba263846cde34ac9067db8485d94b3
BeLumbreras <b.lumbreras@gmail.com>
2022-08-12 23:05:04 -0500

    Capítulo Strings Terminado

--- curso-javascript-moderno/03-Strings/js/06-app.js
+++ curso-javascript-moderno/03-Strings/js/06-app.js
@@ -1,25 +1,40 @@
 const producto = 'Monitor 20 Pulgadas';
 
 // .repeat te va a permitir repetir una cadena de texto
-const texto = ' en Promoción'.repeat(2.7);
+const texto = ' en Promoción'.repeat(3);
 
-console.log(texto);
 console.log(`${producto}${texto}!!!`);
 
 // .split, dividir un string
 const actividad = "Estoy aprendiendo Javascript Moderno";
 console.log(actividad.split(" "));
 
-const hobbies = "Leer, caminar, escuchar música, escribir, aprender a programar";
+const hobbies = "Leer, Caminar, Escuchar Música, Escribir, Running";
 console.log(hobbies.split(", "));
 const hobbiesArray = hobbies.split(", ");
 
 document.body.innerHTML = `
+<h1>Mis hobbies</h1>
+<div class="hobbies">
     <ul><li>${hobbiesArray[0]}</li></ul>
     <ul><li>${hobbiesArray[1]}</li></ul>
     <ul><li>${hobbiesArray[2]}</li></ul>
     <ul><li>${hobbiesArray[3]}</li></ul>
-    <ul><li>${hobbiesArray[4]}</li></ul>`;
+    <ul><li>${hobbiesArray[4]}</li></ul>
+</div>`;
 
 const tweet = "Aprendiendo Javascript #JSModernoConJuan";
 console.log(tweet.split("#"));
+
+const tuitear = "Aprendiendo Javascript #JSModernoConJuan #EstudioDeNoche #NoParesDeAprender";
+const tuitsArray = tuitear.split(" #");
+
+document.body.innerHTML = `
+<h1>Hashtags</h1>
+<div class="hobbies">
+    <ul><li><a href="${tuitsArray[0]}">#${tuitsArray[0]}</a></li></ul>
+    <ul><li><a href="${tuitsArray[1]}">#${tuitsArray[1]}</a></li></ul>
+    <ul><li><a href="${tuitsArray[2]}">#${tuitsArray[2]}</a></li></ul>
+</div>`;
+
+
