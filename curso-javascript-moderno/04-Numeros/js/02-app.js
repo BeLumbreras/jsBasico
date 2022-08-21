d034f635f21a8bc8541f137dbf03671265303871
BeLumbreras <b.lumbreras@gmail.com>
2022-08-20 20:44:53 -0500

    Cambios hasta 20-08-2022

---
+++ curso-javascript-moderno/04-Numeros/js/02-app.js
@@ -0,0 +1,26 @@
+/* OPERACIONES EN JAVASCRIPT */
+
+const num1 = 60;
+const num2 = 30;
+
+let resultado;
+
+// Suma
+resultado = num1 + num2;
+
+// Resta
+resultado = num1 - num2;
+
+// Multiplicación
+resultado = num1 * num2;
+
+// División
+resultado = num1 / num2;
+
+// Módulo
+resultado = num1 % num2;
+
+console.log(resultado);
+
+
+
